5193e1a69ac14319ba0bb6c6a951d292c5678e8b
Nicolás Jofré <nrjofre@miuandes.cl>
2020-04-06 12:30:06 -0400

    1

---
+++ Laboratorio3/Laboratorio3/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio3
+{
+    class Persona
+    {
+        public string rut;
+        public string nombre;
+        public string apellido;
+        public string fecha;
+        public string nacionalidad;
+    }
+}
---
+++ Laboratorio3/Laboratorio3/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio3
+{
+    class Producto
+    {
+        public string nombre;
+        public int precio;
+        public string marca;
+        public int stock;
+
+    }
+}
